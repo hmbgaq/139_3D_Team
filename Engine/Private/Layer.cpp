60b0fc24cd3688d5f891ad2f319ab31c66afab5b
hmbgaq1 <hmbgaq1@gmail.com>
2024-02-08 23:33:45 +0900

    GameObject m_bDead

--- Engine/Private/Layer.cpp
+++ Engine/Private/Layer.cpp
@@ -46,10 +46,25 @@ void CLayer::Tick(_float fTimeDelta)
 
 void CLayer::Late_Tick(_float fTimeDelta)
 {
-	for (auto& pGameObject : m_GameObjects)
+	//for (auto& pGameObject : m_GameObjects)
+	//{
+	//	if (nullptr != pGameObject)
+	//		pGameObject->Late_Tick(fTimeDelta);
+	//}
+
+	for (auto iter = m_GameObjects.begin(); iter != m_GameObjects.end();)
 	{
-		if (nullptr != pGameObject)
-			pGameObject->Late_Tick(fTimeDelta);
+		(*iter)->Late_Tick(fTimeDelta);
+
+		if ((*iter)->Is_Dead())
+		{
+			Safe_Release((*iter));
+			iter = m_GameObjects.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
 	}
 }
 
