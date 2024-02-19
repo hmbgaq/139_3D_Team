f8c71a436b98166b9ba850f349ae8a9ba80f1a99
MyungBin Han <hmbgaq1@gmail.com>
2024-02-20 02:08:16 +0900

    a

--- Engine/Private/PhysXManager.cpp
+++ Engine/Private/PhysXManager.cpp
@@ -8,6 +8,8 @@ CPhysXManager::CPhysXManager(ID3D11Device* pDevice, ID3D11DeviceContext* pContex
 	: m_pDevice(pDevice)
 	, m_pContext(pContext)
 {
+	Safe_AddRef(m_pDevice);
+	Safe_AddRef(m_pContext);
 }
 
 void CPhysXManager::Register_PhysXCollider(CPhysXCollider* pPhysXCollider)
@@ -17,15 +19,30 @@ void CPhysXManager::Register_PhysXCollider(CPhysXCollider* pPhysXCollider)
 
 CPhysXCollider* CPhysXManager::Find_PhysXCollider(const _uint iPhysXColliderIndex)
 {
+	auto iter = m_pPhysXCollders.find(iPhysXColliderIndex);
+
+	if (iter != m_pPhysXCollders.end())
+	{
+		return iter->second;
+	}
+
 	return nullptr;
 }
 
 void CPhysXManager::Register_PhysXController(CPhysXController* pPhysXController)
 {
+	m_pPhysXControllers.emplace(pPhysXController->Get_PControllerIndex(), pPhysXController);
 }
 
 CPhysXController* CPhysXManager::Find_PhysXController(const _uint iPhysXControllerIndex)
 {
+	auto iter = m_pPhysXControllers.find(iPhysXControllerIndex);
+
+	if (iter != m_pPhysXControllers.end())
+	{
+		return iter->second;
+	}
+
 	return nullptr;
 }
 
@@ -57,29 +74,8 @@ HRESULT CPhysXManager::Initialize(const _uint In_iNumLayer)
 	// Create Cooking
 	m_pCooking = PxCreateCooking(PX_PHYSICS_VERSION, *m_pFoundation, PxCookingParams(PxTolerancesScale()));
 
-	// Create Cuda
-	PxCudaContextManagerDesc tCudaDesc;
-	//tCudaDesc.graphicsDevice = DEVICE;
-	//tCudaDesc.interopMode = PxCudaInteropMode::Enum::D3D11_INTEROP;
-	//tCudaDesc.ctx = GET_SINGLE(CCuda_Device)->Get_CudaContext();
-
-	m_pCudaContextManager = PxCreateCudaContextManager(*m_pFoundation, tCudaDesc, PxGetProfilerCallback());
-
-	if (m_pCudaContextManager)
-	{
-		if (!m_pCudaContextManager->contextIsValid())
-		{
-			if (m_pCudaContextManager)
-				m_pCudaContextManager->release();
-			m_pCudaContextManager = nullptr;
-		}
-	}
-
 
 	m_pMaterial = m_pPhysics->createMaterial(0.5f, 0.5f, -10.f);
-
-
-
 
 
 	return S_OK;
@@ -87,6 +83,182 @@ HRESULT CPhysXManager::Initialize(const _uint In_iNumLayer)
 
 void CPhysXManager::Tick(_float fTimeDelta)
 {
+	if (m_fTimeAcc > 1.f)
+	{
+		Garbage_Collector();
+		m_fTimeAcc = 0.f;
+	}
+	else
+	{
+		m_fTimeAcc += fTimeDelta;
+	}
+
+	if (m_pScene)
+	{
+		if (fTimeDelta > 3.f)
+			return;
+
+		if (1 == m_pScene->getTimestamp() ||
+			2 == m_pScene->getTimestamp())
+			fTimeDelta = 0.16f;
+
+		m_pScene->simulate(fTimeDelta);
+		m_pScene->fetchResults(true);
+	}
+
+}
+
+void CPhysXManager::Check_PhysXFilterGroup(const _uint In_iLeftLayer, const _uint In_iRightLayer)
+{
+	_uint iRow = (_uint)In_iLeftLayer;
+	_uint iCol = (_uint)In_iRightLayer; 
+
+	_uint iMax = iCol;
+	if (iRow > iCol)
+	{
+		iMax = iRow;
+	}
+
+	for (_uint i = (_uint)m_arrCheck.size(); i <= iMax; ++i)
+	{
+		m_arrCheck.emplace_back(0);
+	}
+
+	if (m_arrCheck[iRow] & (1 << iCol))
+	{
+		m_arrCheck[iRow] &= ~(1 << iCol); 
+		m_arrCheck[iCol] &= ~(1 << iRow); 
+	}
+	else
+	{
+		m_arrCheck[iRow] |= (1 << iCol);
+		m_arrCheck[iCol] |= (1 << iRow);
+	}
+}
+
+_uint CPhysXManager::Get_PhysXFilterGroup(const _uint In_iIndex)
+{
+	return m_arrCheck[In_iIndex];
+}
+
+PxRigidDynamic* CPhysXManager::Create_DynamicActor(const PxTransform& transform, const PxGeometry& geometry, PxMaterial* pMaterial)
+{
+	return PxCreateDynamic(*m_pPhysics, transform, geometry, *pMaterial, 10.f);
+}
+
+PxRigidDynamic* CPhysXManager::Create_DynamicActor(const PxTransform& transform)
+{
+	return m_pPhysics->createRigidDynamic(transform);
+}
+
+PxRigidStatic* CPhysXManager::Create_StaticActor(const PxTransform& transform, const PxGeometry& geometry, PxMaterial* pMaterial)
+{
+	return PxCreateStatic(*m_pPhysics, transform, geometry, *pMaterial);
+}
+
+PxRigidStatic* CPhysXManager::Create_StaticActor(const PxTransform& transform)
+{
+	return m_pPhysics->createRigidStatic(transform);
+}
+
+void CPhysXManager::Create_ConvexMesh(PxVec3** pVertices, _uint iNumVertice, PxConvexMesh** ppOut)
+{
+	PxCookingParams params = m_pCooking->getParams();
+
+	// Use the new (default) PxConvexMeshCookingType::eQUICKHULL
+	params.convexMeshCookingType = PxConvexMeshCookingType::eQUICKHULL;
+
+	// If the gaussMapLimit is chosen higher than the number of output vertices, no gauss map is added to the convex mesh data (here 256).
+	// If the gaussMapLimit is chosen lower than the number of output vertices, a gauss map is added to the convex mesh data (here 16).
+	params.gaussMapLimit = 16;
+	params.buildGPUData = true;
+	m_pCooking->setParams(params);
+
+	// Setup the convex mesh descriptor
+	PxConvexMeshDesc Desc;
+
+	// We provide points only, therefore the PxConvexFlag::eCOMPUTE_CONVEX flag must be specified
+	Desc.points.data = *pVertices;
+	Desc.points.count = iNumVertice;
+	Desc.points.stride = sizeof(PxVec3);
+	Desc.flags = PxConvexFlag::eCOMPUTE_CONVEX;
+
+	*ppOut = m_pCooking->createConvexMesh(Desc, m_pPhysics->getPhysicsInsertionCallback());
+}
+
+void CPhysXManager::Create_ConvexMesh(const PxConvexMeshDesc& In_MeshDesc, PxConvexMesh** ppOut)
+{
+	PxCookingParams params = m_pCooking->getParams();
+
+	// Use the new (default) PxConvexMeshCookingType::eQUICKHULL
+	params.convexMeshCookingType = PxConvexMeshCookingType::eQUICKHULL;
+
+	// If the gaussMapLimit is chosen higher than the number of output vertices, no gauss map is added to the convex mesh data (here 256).
+	// If the gaussMapLimit is chosen lower than the number of output vertices, a gauss map is added to the convex mesh data (here 16).
+	params.gaussMapLimit = 16;
+	params.buildGPUData = true;
+	m_pCooking->setParams(params);
+
+	*ppOut = m_pCooking->createConvexMesh(In_MeshDesc, m_pPhysics->getPhysicsInsertionCallback());
+
+}
+
+void CPhysXManager::Create_Shape(const PxGeometry& Geometry, PxMaterial* pMaterial, const _bool isExculsive, const PxShapeFlags In_ShapeFlags, PxShape** ppOut)
+{
+	*ppOut = m_pPhysics->createShape(Geometry, *pMaterial, isExculsive, In_ShapeFlags);
+}
+
+void CPhysXManager::Create_MeshFromTriangles(const PxTriangleMeshDesc& In_MeshDesc, PxTriangleMesh** ppOut)
+{
+	PxCookingParams params = m_pCooking->getParams();
+
+	// Use the new (default) PxConvexMeshCookingType::eQUICKHULL
+	params.buildGPUData = true;
+
+	m_pCooking->setParams(params);
+
+
+	PxDefaultMemoryOutputStream writeBuffer;
+	PxTriangleMeshCookingResult::Enum result;
+	bool status = m_pCooking->cookTriangleMesh(In_MeshDesc, writeBuffer, &result);
+	if (!status)
+		DEBUG_ASSERT;
+
+	PxDefaultMemoryInputData readBuffer(writeBuffer.getData(), writeBuffer.getSize());
+	*ppOut = m_pPhysics->createTriangleMesh(readBuffer);
+}
+
+void CPhysXManager::Create_Controller(const PxCapsuleControllerDesc& In_ControllerDesc, PxController** ppOut)
+{
+	*ppOut = m_pControllerManager->createController(In_ControllerDesc);
+}
+
+
+void CPhysXManager::Garbage_Collector()
+{
+	for (auto iter = m_pPhysXCollders.begin(); iter != m_pPhysXCollders.end();)
+	{
+		if (nullptr == (*iter).second)
+		{
+			iter = m_pPhysXCollders.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
+
+	for (auto iter = m_pPhysXControllers.begin(); iter != m_pPhysXControllers.end();)
+	{
+		if (nullptr == (*iter).second)
+		{
+			iter = m_pPhysXControllers.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
 }
 
 CPhysXManager* CPhysXManager::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, _uint iNumLevels)
@@ -96,4 +268,31 @@ CPhysXManager* CPhysXManager::Create(ID3D11Device* pDevice, ID3D11DeviceContext*
 
 void CPhysXManager::Free()
 {
+	PX_UNUSED(true);
+
+	if (m_pControllerManager)
+		m_pControllerManager->release();
+
+	if (m_pScene)
+		m_pScene->release();
+
+	if (m_pPhysics)
+		m_pPhysics->release();
+
+	if (m_pCooking)
+		m_pCooking->release();
+
+	if (m_pPVD)
+	{
+		PxPvdTransport* transport = m_pPVD->getTransport();
+		m_pPVD->release();
+		m_pPVD = nullptr;
+		transport->release();
+	}
+
+	if (m_pFoundation)
+		m_pFoundation->release();
+
+	Safe_Release(m_pDevice);
+	Safe_Release(m_pContext);
 }
--- Reference/Public/PhysXManager.h
+++ Reference/Public/PhysXManager.h
@@ -25,9 +25,30 @@ public:
 	HRESULT	Initialize(const _uint In_iNumLayer);
 	void	Tick(_float fTimeDelta);
 
+public:
+	void			Check_PhysXFilterGroup(const _uint In_iLeftLayer, const _uint In_iRightLayer);
+	_uint			Get_PhysXFilterGroup(const _uint In_iIndex);
+
+	PxRigidDynamic* Create_DynamicActor(const PxTransform& transform, const PxGeometry& geometry, PxMaterial* pMaterial = nullptr);
+	PxRigidDynamic* Create_DynamicActor(const PxTransform& transform);
+	PxRigidStatic* Create_StaticActor(const PxTransform& transform, const PxGeometry& geometry, PxMaterial* pMaterial = nullptr);
+	PxRigidStatic* Create_StaticActor(const PxTransform& transform);
+
+	void			Create_ConvexMesh(PxVec3** pVertices, _uint iNumVertice, PxConvexMesh** ppOut);
+	void			Create_ConvexMesh(const PxConvexMeshDesc& In_MeshDesc, PxConvexMesh** ppOut);
+	void			Create_Shape(const PxGeometry& Geometry, PxMaterial* pMaterial, const _bool isExculsive, const PxShapeFlags In_ShapeFlags, PxShape** ppOut);
+	void			Create_MeshFromTriangles(const PxTriangleMeshDesc& In_MeshDesc, PxTriangleMesh** ppOut);
+	void			Create_Controller(const PxCapsuleControllerDesc& In_ControllerDesc, PxController** ppOut);
+
+
+
+private:
+	void	Garbage_Collector();
+
 
 private:
 	vector<_uint>			m_arrCheck;
+	_float					m_fTimeAcc = { 0.f };
 
 private:
 	// Foundation을 생성하는데 필요한 변수
@@ -36,14 +57,12 @@ private:
 
 	PxFoundation* m_pFoundation;
 	PxPhysics* m_pPhysics;
-	PxScene* m_pScenes;
+	PxScene* m_pScene;
 	PxMaterial* m_pMaterial;
 
 	PxRigidDynamic* m_pTemp = nullptr;
 	PxControllerManager* m_pControllerManager = nullptr;
 	
-
-
 
 	////Visual Debugger
 	PxPvd* m_pPVD;
