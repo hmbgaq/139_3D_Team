24a6e124de97aa01b29cfc29871fbbc1f7173afb
JangJunHo1992 <wnsghxn@naver.com>
2024-03-01 10:51:54 +0900

    파싱및 파싱사용법 주석으로 걸어둠

--- Client/Private/Player_MeleeCombo_01.cpp
+++ Client/Private/Player_MeleeCombo_01.cpp
@@ -3,12 +3,13 @@
 #include "Player_Empowered_Idle.h"
 #include "Effect.h"
 #include "Clone_Manager.h"
+#include "Bone.h"
 
 void CPlayer_MeleeCombo_01::Initialize(CPlayer* pActor)
 {
 	__super::Initialize(pActor);
-	string Test = "../Bin/DataFiles/Data_Animation/";
-	pActor->Set_EventNotify(Test, "Test2_AnimationData");
+	string Test = "Data_Animation/";
+	pActor->Set_EventNotify(Test, "Test2_AnimationData.json");
 	pActor->Set_Animation(g_iAnimIndex, CModel::ANIM_STATE_NORMAL, true);
 }
 
@@ -23,8 +24,13 @@ CState<CPlayer>* CPlayer_MeleeCombo_01::Update(CPlayer* pActor, _float fTimeDelt
 		{
 			if (pActor->Get_TrackPosition() >= pActor->Get_CharcterDesc().EffectOnTrackPosition)
 			{
+				//왼손 오른손 무기화 시켜서 그 무기의 좌표를 여기에 있는 셋 포지션안에 집어 넣으면 그위치에서 터질거임 지금 로컬 값으로 되어 있어서 0점 근처에서만터짐
 				CEffect* pEffect = CClone_Manager::GetInstance()->Create_Effect(m_pGameInstance->Get_NextLevel(), LAYER_EFFECT, pActor->Get_CharcterDesc().EffectFileName + ".json");
-				pEffect->Set_Position(pActor->Get_CharcterDesc().EffectPosition);
+				_float3 EffectBonePosition = {};
+				EffectBonePosition.x = pActor->Get_Body()->Get_BonePtr("RightHandIK")->Get_CombinedTransformationFloat4x4()._41;
+				EffectBonePosition.y = pActor->Get_Body()->Get_BonePtr("RightHandIK")->Get_CombinedTransformationFloat4x4()._42;
+				EffectBonePosition.z = pActor->Get_Body()->Get_BonePtr("RightHandIK")->Get_CombinedTransformationFloat4x4()._43;
+				pEffect->Set_Position(EffectBonePosition);
 				pActor->bTest = false;
 			}
 		}
--- Engine/Private/Character.cpp
+++ Engine/Private/Character.cpp
@@ -170,8 +170,8 @@ HRESULT CCharacter::Add_Weapon(const wstring& strPrototypeTag, string strBoneNam
 
 void CCharacter::Set_EventNotify(string strPath, string JsonFileName)
 {
-	/*string strPath = "../Bin/DataFiles/Data_Animation/";*/
-	LoadAnimJson(strPath, JsonFileName);
+	string strpath = "../../Client/Bin/DataFiles/"+strPath;
+	LoadAnimJson(strpath, JsonFileName);
 }
 
 HRESULT CCharacter::LoadAnimJson(string strPath, string strFileName)
@@ -190,8 +190,7 @@ HRESULT CCharacter::LoadAnimJson(string strPath, string strFileName)
 	json WeaponJson = LoadJson["Weapon"];
 	json EffectJson = LoadJson["Effect"];
 	
-	CharAnimDesc.Body = LoadJson["Body"];
-	CharAnimDesc.BoneName = WeaponJson["BoneName"];
+	//CharAnimDesc.Body = LoadJson["Body"];
 	CharAnimDesc.AnimationIndex = BodyJson["AnimIndex"];
 	CharAnimDesc.Duration = BodyJson["AnimDuration"];
 	CharAnimDesc.EventTrackposition = BodyJson["CurrentTrackPosition"];
@@ -204,6 +203,7 @@ HRESULT CCharacter::LoadAnimJson(string strPath, string strFileName)
 
 	if (WeaponJson != nullptr)
 	{
+		CharAnimDesc.BoneName = WeaponJson["BoneName"];
 		CharAnimDesc.EventWeaponTrackPosition = BodyJson["CurrentTrackPosition"];
 		CharAnimDesc.ColliderWeaponSize = WeaponJson["WeaponColliderSize"];
 		CharAnimDesc.ColliderWeaponTrackPositionOn = WeaponJson["WeaponColliderTrackPositionOn"];
