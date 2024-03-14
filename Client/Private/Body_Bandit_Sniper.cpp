2e5e968129b44378f45ba21ff7f9ea0ce38ca77e
SO_YOUNG <chu0326@naver.com>
2024-03-14 14:27:46 +0900

    [소영] 병합전 커밋

--- Client/Private/Body_Bandit_Sniper.cpp
+++ Client/Private/Body_Bandit_Sniper.cpp
@@ -34,6 +34,16 @@ void CBody_Bandit_Sniper::Priority_Tick(_float fTimeDelta)
 void CBody_Bandit_Sniper::Tick(_float fTimeDelta)
 {
 	__super::Tick(fTimeDelta);
+
+	if (m_pGameInstance->Key_Down(DIK_K))
+	{
+		iDiscardMeshNumber += 1;
+		if (iDiscardMeshNumber > m_pModelCom->Get_NumMeshes())
+		{
+			iDiscardMeshNumber = 0;
+		}
+		cout << iDiscardMeshNumber << endl;
+	}
 }
 
 void CBody_Bandit_Sniper::Late_Tick(_float fTimeDelta)
@@ -49,6 +59,9 @@ HRESULT CBody_Bandit_Sniper::Render()
 
 	for (size_t i = 0; i < iNumMeshes; i++)
 	{
+		if (i == iDiscardMeshNumber)
+			continue;
+
 		m_pModelCom->Bind_BoneMatrices(m_pShaderCom, "g_BoneMatrices", (_uint)i);
 
 		m_pModelCom->Bind_ShaderResource(m_pShaderCom, "g_DiffuseTexture", (_uint)i, aiTextureType_DIFFUSE);
@@ -65,7 +78,7 @@ HRESULT CBody_Bandit_Sniper::Render()
 
 HRESULT CBody_Bandit_Sniper::Render_Shadow()
 {
-	_float lightFarValue = m_pGameInstance->Get_ShadowLightFar(m_pGameInstance->Get_NextLevel());
+	_float lightFarValue = m_pGameInstance->Get_ShadowLightFar(m_iCurrnetLevel);
 	_uint iNumMeshes = m_pModelCom->Get_NumMeshes();
 
 	FAILED_CHECK(m_pShaderCom->Bind_RawValue("g_fLightFar", &lightFarValue, sizeof(_float)));
@@ -121,6 +134,7 @@ HRESULT CBody_Bandit_Sniper::Bind_ShaderResources()
 	FAILED_CHECK(__super::Bind_ShaderResources());
 
 	_float fCamFar = m_pGameInstance->Get_CamFar();
+
 	FAILED_CHECK(m_pShaderCom->Bind_RawValue("g_fCamFar", &fCamFar, sizeof(_float)));
 
 	return S_OK;
--- Client/Private/Weapon_Bandit_Sniper.cpp
+++ Client/Private/Weapon_Bandit_Sniper.cpp
@@ -24,20 +24,11 @@ HRESULT CWeapon_Bandit_Sniper::Initialize(void* pArg)
 {
 	FAILED_CHECK(__super::Initialize(pArg));
 
-	string path = "../Bin/DataFiles/Data_Monster/Sniper/Weapon.json";
-	json In_Json;
-	CJson_Utility::Load_Json(path.c_str(), In_Json);
-	m_pTransformCom->Load_FromJson(In_Json);
-		
+	FAILED_CHECK(Load_Json());
+
+	FAILED_CHECK(Option_Setting());
+
 	return S_OK;
-}
-
-void CWeapon_Bandit_Sniper::Sniping(_float4 vTargetPos, _float3 StartfPos)
-{
-	CGameObject* pBullet = m_pGameInstance->Add_CloneObject_And_Get(m_iCurrnetLevel, LAYER_MONSTER_BULLET, L"Prototype_GameObject_Bullet_Bandit_Sniper");
-
-	pBullet->Set_Position(StartfPos);
-	pBullet->Get_Transform()->Look_At(vTargetPos);
 }
 
 HRESULT CWeapon_Bandit_Sniper::Ready_Components()
@@ -49,6 +40,23 @@ HRESULT CWeapon_Bandit_Sniper::Ready_Components()
 
 	/* For. Com_Shader */
 	FAILED_CHECK(__super::Add_Component(iNextLevel, TEXT("Prototype_Component_Shader_Model"), TEXT("Com_Shader"), reinterpret_cast<CComponent**>(&m_pShaderCom)));
+
+	return S_OK;
+}
+
+HRESULT CWeapon_Bandit_Sniper::Load_Json()
+{
+	string path = "../Bin/DataFiles/Data_Monster/Sniper/Weapon.json";
+	json In_Json;
+	CJson_Utility::Load_Json(path.c_str(), In_Json);
+	m_pTransformCom->Load_FromJson(In_Json);
+
+	return S_OK;
+}
+
+HRESULT CWeapon_Bandit_Sniper::Option_Setting()
+{
+	iRenderPass = ECast(MONSTER_SHADER::COMMON_ORIGIN);
 
 	return S_OK;
 }
@@ -94,9 +102,32 @@ HRESULT CWeapon_Bandit_Sniper::Bind_ShaderResources()
 
 HRESULT CWeapon_Bandit_Sniper::Render()
 {
-	FAILED_CHECK(__super::Render());
+	FAILED_CHECK(Bind_ShaderResources());
+
+	_uint		iNumMeshes = m_pModelCom->Get_NumMeshes();
+
+	for (size_t i = 0; i < iNumMeshes; i++)
+	{
+		m_pModelCom->Bind_BoneMatrices(m_pShaderCom, "g_BoneMatrices", (_uint)i);
+
+		m_pModelCom->Bind_ShaderResource(m_pShaderCom, "g_DiffuseTexture", (_uint)i, aiTextureType_DIFFUSE);
+		m_pModelCom->Bind_ShaderResource(m_pShaderCom, "g_NormalTexture", (_uint)i, aiTextureType_NORMALS);
+		m_pModelCom->Bind_ShaderResource(m_pShaderCom, "g_SpecularTexture", (_uint)i, aiTextureType_SPECULAR);
+
+		m_pShaderCom->Begin(iRenderPass);
+
+		m_pModelCom->Render((_uint)i);
+	}
 
 	return S_OK;
+}
+
+void CWeapon_Bandit_Sniper::Sniping(_float4 vTargetPos, _float3 StartfPos)
+{
+	CGameObject* pBullet = m_pGameInstance->Add_CloneObject_And_Get(m_iCurrnetLevel, LAYER_MONSTER_BULLET, L"Prototype_GameObject_Bullet_Bandit_Sniper");
+
+	pBullet->Set_Position(StartfPos);
+	pBullet->Get_Transform()->Look_At(vTargetPos);
 }
 
 #pragma region Create, Clone, Pool, Free
