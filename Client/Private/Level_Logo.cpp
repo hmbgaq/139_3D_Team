e2f64498a43ca63dcf0062fce7d1ca4d104c1368
roal3047 <roal3047@naver.com>
2024-03-15 16:16:51 +0900

    녹화전 커밋 프레임 수정

--- Client/Private/Level_Logo.cpp
+++ Client/Private/Level_Logo.cpp
@@ -61,8 +61,8 @@ void CLevel_Logo::Tick(_float fTimeDelta)
 		if (iCheckPoint == IDOK)
 		{
 			//FAILED_CHECK_RETURN(m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, LEVEL_INTRO)),);
-			//FAILED_CHECK_RETURN(m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, LEVEL_GAMEPLAY)),);
-			FAILED_CHECK_RETURN(m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, LEVEL_INTRO_BOSS)),);
+			FAILED_CHECK_RETURN(m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, LEVEL_GAMEPLAY)),);
+			//FAILED_CHECK_RETURN(m_pGameInstance->Open_Level(LEVEL_LOADING, CLevel_Loading::Create(m_pDevice, m_pContext, LEVEL_INTRO_BOSS)),);
 		}
 		else if (iCheckPoint == IDCANCEL)
 		{
