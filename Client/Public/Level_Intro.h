8c9b7444377aefba1be393ea84bfc78a7a31177e
SO_YOUNG <chu0326@naver.com>
2024-03-15 16:08:54 +0900

    몬스터 쳐맞음

--- Client/Public/Level_Intro.h
+++ Client/Public/Level_Intro.h
@@ -3,9 +3,6 @@
 #include "Client_Defines.h"
 #include "Level.h"
 
-/* -------------- Memo --------------- */
-// Intro¾Æ´Ô Å×½ºÆ®¸ÊÀÓ 
-/* --------------------------------- */
 BEGIN(Client)
 
 class CLevel_Intro final : public CLevel
@@ -24,16 +21,17 @@ private:
 	HRESULT Ready_Layer_Camera(const wstring& strLayerTag);
 	HRESULT Ready_Layer_Effect(const wstring& strLayerTag);
 	HRESULT Ready_Layer_BackGround(const wstring& strLayerTag);
-	HRESULT Ready_Layer_Player(const wstring& strLayerTag);
-	HRESULT Ready_Layer_Monster(const wstring& strLayerTag);
+	HRESULT Ready_LandObjects();
+	HRESULT Ready_Layer_Player(const wstring& strLayerTag, void* pArg);
+	HRESULT Ready_Layer_Monster(const wstring& strLayerTag, void* pArg);
+	HRESULT Ready_Layer_Building(const wstring& strLayerTag, void* pArg);
 	HRESULT Ready_Layer_Test(const wstring& strLayerTag);
 	HRESULT Ready_Shader();
 
-#pragma region UI
-private:
+private: /* UI */
 	HRESULT Ready_UI();
 	HRESULT Ready_Layer_UI_Monster(const wstring& strLayerTag, void* pArg);
-	/* Interface */
+#pragma region Interface
 	HRESULT Ready_Layer_UI_Interface(const wstring& strLayerTag, void* pArg);
 	HRESULT Ready_LeftInterface(const wstring& strLayerTag, void* pArg);
 	HRESULT Ready_RightInterface(const wstring& strLayerTag, void* pArg);
@@ -41,15 +39,12 @@ private:
 	HRESULT Ready_TutorialBox(const wstring& strLayerTag, void* pArg);
 	HRESULT Ready_LevelUp(const wstring& strLayerTag, void* pArg);
 	HRESULT Ready_Reward_Item(const wstring& strLayerTag, void* pArg);
-	HRESULT Ready_Layer_UI(const wstring& strLayerTag, void* pArg);
-	/* Cursor */
-	HRESULT Ready_Cursor(const wstring& strLayerTag, void* pArg);
 #pragma endregion End
+	HRESULT Ready_Layer_UI(const wstring& strLayerTag, void* pArg);
 
 private:
 	_int			TempLightNumber = 0;
-
-	string			m_strStage1MapLoadPath = "../Bin/DataFiles/Data_Map/Stage1Final_MonsterInclude.json";
+	string			m_strStage1MapLoadPath = "../Bin/DataFiles/Data_Map/Stage1Boss_MapData_MapData.json";
 
 public:
 	static CLevel_Intro* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
