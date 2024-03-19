a2180fae354f02d423e6c4946fdadaadab98b98e
SO_YOUNG <chu0326@naver.com>
2024-03-19 20:19:37 +0900

    추가 애니메이션 넣기

--- Client/Public/Sniper_Burn_Loop_TEMP.h
+++ Client/Public/Sniper_Burn_Loop_TEMP.h
@@ -1,5 +1,17 @@
 #pragma once
-class CSniper_Burn_Loop_TEMP
+#include "Bandit_Sniper_State_Hit.h"
+
+BEGIN(Client)
+class CSniper_Burn_Loop_TEMP : public CBandit_Sniper_State_Hit
 {
+public:
+	virtual void Initialize(CBandit_Sniper* pActor) override;
+	virtual CState<CBandit_Sniper>* Update(CBandit_Sniper* pActor, _float fTimeDelta) override;
+	virtual void Release(CBandit_Sniper* pActor) override;
+
+public:
+	static const _uint g_iAnimIndex = CBandit_Sniper::Bandit_Sniper_State::Sniper_Burn_Loop_TEMP;
+
 };
 
+END
