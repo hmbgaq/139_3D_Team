8a9c189e1147cfd64ee5945efed093ad1ba9ec1c
SO_YOUNG <chu0326@naver.com>
2024-04-08 20:21:33 +0900

    창크기 조절

--- Client/Private/Camera_Dynamic.cpp
+++ Client/Private/Camera_Dynamic.cpp
@@ -83,10 +83,10 @@ void CCamera_Dynamic::Tick(_float fTimeDelta)
 			CData_Manager::GetInstance()->Get_MasterCamera()->Set_CameraType(CMasterCamera::SpringCamera);
 	}
 
-	//if (m_pGameInstance->Key_Down(DIK_NUMPAD9))
-	//{
-	//	m_pGameInstance->Get_Player()->Set_InitPosition(m_pTransformCom->Get_Position());
-	//}
+	if (m_pGameInstance->Key_Down(DIK_NUMPAD9))
+	{
+		m_pGameInstance->Get_Player()->Set_InitPosition(m_pTransformCom->Get_Position());
+	}
 
 	__super::Tick(fTimeDelta);
 }
