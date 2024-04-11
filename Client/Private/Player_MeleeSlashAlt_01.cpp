9b88d35217130e4e8e45953cd970db0e16e2564c
MyungBin Han <hmbgaq1@gmail.com>
2024-04-11 12:44:53 +0900

    melee combo 수정

--- Client/Private/Player_MeleeSlashAlt_01.cpp
+++ Client/Private/Player_MeleeSlashAlt_01.cpp
@@ -8,32 +8,65 @@
 void CPlayer_MeleeSlashAlt_01::Initialize(CPlayer* pActor)
 {
 	__super::Initialize(pActor);
-	pActor->Set_Animation(g_iAnimIndex, CModel::ANIM_STATE_NORMAL, true);
-
-	//string Test = "Data_Animation/";
-	//pActor->Set_EventNotify(Test, "Test2_AnimationData.json");
+	pActor->Set_Animation(g_iAnimIndex, CModel::ANIM_STATE_NORMAL, true, true, 0);
 
 
-	//CWeapon* pWeapon = pActor->Get_Weapon(TEXT("Weapon_Punch_R"));
+	CWeapon* pWeapon = pActor->Get_Weapon(PLAYER_WEAPON_PUNCH_R);
 
-	//pWeapon
-	//	->Set_Damage(0.f)
-	//	->Set_Direction(Direction::Right)
-	//	->Set_Power(Power::Medium)
-	//	->Set_Force(0.0f);
+	pWeapon
+		->Set_Damage(15.f)
+		->Set_Direction(Direction::Front)
+		->Set_Power(Power::Medium)
+		->Set_Force(0.0f);
 
-	//pWeapon->Set_Enable(true);
+	pWeapon->Set_Enable(true);
+	pWeapon->Set_Enable_Collisions(false);
 
 }
 
 CState<CPlayer>* CPlayer_MeleeSlashAlt_01::Update(CPlayer* pActor, _float fTimeDelta)
 {
 	__super::Update(pActor, fTimeDelta);
+	
+	if (false == m_bFlags[0])
+	{
+		m_bFlags[0] = pActor->Is_Inputable_Front(16);
+		if (true == m_bFlags[0])
+		{
+			CWeapon* pWeapon = pActor->Set_Weapon_Collisions_Enable(PLAYER_WEAPON_PUNCH_R, true);
+		}
+	}
+	else if (false == m_bFlags[1])
+	{
+		pActor->Chasing_Attack(fTimeDelta);
+		m_bFlags[1] = pActor->Is_Inputable_Front(23);
+		if (true == m_bFlags[1])
+		{
+			CWeapon* pWeapon = pActor->Set_Weapon_Collisions_Enable(PLAYER_WEAPON_PUNCH_R, false);
+		}
 
-	if (pActor->Is_Inputable_Front(28))
+	}
+	else if (false == m_bFlags[2])
+	{
+		m_bFlags[2] = pActor->Is_Inputable_Front(30);
+		//if (true == m_bFlags[2])
+		//{
+		//	
+		//}
+
+	}
+	else
 	{
 		return __super::Update_State(pActor, fTimeDelta, g_iAnimIndex);
 	}
+
+
+
+	//if (pActor->Is_Inputable_Front(28))
+	//{
+	//	return __super::Update_State(pActor, fTimeDelta, g_iAnimIndex);
+	//}
+
 	return nullptr;
 }
 
@@ -41,6 +74,7 @@ void CPlayer_MeleeSlashAlt_01::Release(CPlayer* pActor)
 {
 	__super::Release(pActor);
 
-	//CWeapon* pWeapon = pActor->Get_Weapon(TEXT("Weapon_Punch_R"));
-	//pWeapon->Set_Enable(false);
+	CWeapon* pWeapon = pActor->Set_Weapon_Enable(PLAYER_WEAPON_PUNCH_R, false);
+	pActor->Set_Target(nullptr);
+
 }
