685892a6c1da4305734d7d85756a5218cefac324
marujoy01 <marujoy3@gmail.com>
2024-04-13 06:26:43 +0900

    [성희] 커밋 수정본

--- Client/Private/Level_Logo.cpp
+++ Client/Private/Level_Logo.cpp
@@ -42,7 +42,7 @@ HRESULT CLevel_Logo::Initialize()
 	ShowCursor(false);
 
 
-	FAILED_CHECK(EFFECT_MANAGER->Ready_EffectPool()); // ÀÌÆåÆ® Ç®
+	//ÁÖ¼® FAILED_CHECK(EFFECT_MANAGER->Ready_EffectPool()); // ÀÌÆåÆ® Ç®
 
 	return S_OK;
 }
@@ -69,7 +69,7 @@ void CLevel_Logo::Tick(_float fTimeDelta)
 		m_pDataManager->Get_SelectLevel() != LEVEL_END &&
 		m_pDataManager->Get_SelectLevel() != LEVEL_TOOL)
 	{
-		FAILED_CHECK_RETURN(Ready_Static_UI(), );
+		//FAILED_CHECK_RETURN(Ready_Static_UI(), );
 	}
 
 	switch (m_pDataManager->Get_SelectLevel())
